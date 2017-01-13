3c90903ad4e3fb83f24d649221d18f87d271bf90
Solor <gmhayato8810@gmail.com>
2017-01-14 00:28:05 +0900

    fix bugs

--- etc/init/assets/apt/app-cli.sh
+++ etc/init/assets/apt/app-cli.sh
@@ -48,30 +48,30 @@ cat <<EOF
 ###############################
 EOF
 
-if ! contains "${SHELL:-}" "zsh"; then
-    zsh_path="$(which zsh)"
 
-    # Check /etc/shells
-    if ! grep -xq "${zsh_path:=/bin/zsh}" /etc/shells; then
-        exit 1
-    fi
 
-    if [ -x "$zsh_path" ]; then
-        # Changing for a general user
-        if chsh -s "$zsh_path" "${USER:-root}"; then
-            echo "Change shell to $zsh_path for ${USER:-root} successfully"
-        else
-            exit 1
-        fi
+zsh_path="$(which zsh)"
 
-        # For root user
-        if [ ${EUID:-${UID}} = 0 ]; then
-            if chsh -s "$zsh_path" && :; then
-                echo "[root] changes shell to $zsh_path successfully"
-            fi
-        fi
+# Check /etc/shells
+if ! grep -xq "${zsh_path:=/bin/zsh}" /etc/shells; then
+    exit 1
+fi
+
+if [ -x "$zsh_path" ]; then
+    # Changing for a general user
+    if chsh -s "$zsh_path" "${USER:-root}"; then
+        echo "Change shell to $zsh_path for ${USER:-root} successfully"
     else
-        echo "$zsh_path: invalid path"
         exit 1
     fi
+
+    # For root user
+    if [ ${EUID:-${UID}} = 0 ]; then
+        if chsh -s "$zsh_path" && :; then
+            echo "[root] changes shell to $zsh_path successfully"
+        fi
+    fi
+else
+    echo "$zsh_path: invalid path"
+    exit 1
 fi
